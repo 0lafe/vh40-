ba26602dba6c7c541f83c2415269ce31a1171d23
0lafe <0lafethehorsescrub@gmail.com>
2024-08-13 18:06:05 -0500

    initial commit

---
+++ playertweakdata.lua
@@ -0,0 +1,6 @@
+function PlayerTweakData:_set_difficulty_4()
+	self.damage.automatic_respawn_time = 90
+	self.damage.DOWNED_TIME_DEC = 15
+	self.damage.DOWNED_TIME_MIN = 1
+	self.damage.MIN_DAMAGE_INTERVAL = 0.15
+end
